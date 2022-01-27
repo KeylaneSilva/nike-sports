8d95b2ae32b5fba8dae771d585a0a75feb67c3a6
Keylane <keylanessilva07@gmail.com>
2022-01-26 23:38:15 -0300

    nike-sports

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function imgSlider(imagens){
+    document.querySelector('.nike').src = imagens
+}
+
+function trocarFundo(cor){
+   const col2 = document.querySelector('.col-2')
+   col2.style.background = cor
+}
